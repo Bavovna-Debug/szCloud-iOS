8c15772d2af774a58df2dfdb54c612121a067c86
Maxim Shchetynin <maxim@zeppelinium.de>
2018-06-23 15:17:55 +0200

    UI Anpassungen

---
+++ szCloud/SettingsViewController.m
@@ -0,0 +1,15 @@
+//
+//  szCloud
+//
+//  Copyright (c) 2014 Meine Werke. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+@end
